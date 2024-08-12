f7cd9066141f96d721d7491b30187b79fad14d3b
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-08-12 13:28:55 +0330

    Add Flash Ing Landing

--- src/pages/site/Home.tsx
+++ src/pages/site/Home.tsx
@@ -1,10 +1,12 @@
 import Landing from '@/assets/components/site/home/landing/Landing'
+import LatesCourses from '@/assets/components/site/home/latesCourses/LatesCourses'
 import { memo } from 'react'
 
 const Home = memo(() => {
   return (
     <div id="home">
       <Landing/>
+      <LatesCourses/>
     </div>
   )
 })
