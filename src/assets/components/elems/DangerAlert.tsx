c1d101bf0348a43125eb27ce8fb9f59f3b5c3a63
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-08-27 16:23:07 +0330

    Create DangerAlert

---
+++ src/assets/components/elems/DangerAlert.tsx
@@ -0,0 +1,13 @@
+import { memo } from "react";
+
+type Props = {
+  text?: string;
+};
+
+const DangerAlert = memo(({ text = "مشکلی پیش آمده ..." }: Props) => (
+  <div className="bg-red-100 dark:bg-red-200 text-red-900 sm:text-lg md:text-xl font-bold font-yekan px-4 py-8 rounded-lg shadow">
+    {text}
+  </div>
+));
+
+export default DangerAlert;
--- src/assets/components/site/coursePreview/courseDetailsWrapper/mainDetails/sessions/Sessions.tsx
+++ src/assets/components/site/coursePreview/courseDetailsWrapper/mainDetails/sessions/Sessions.tsx
@@ -2,6 +2,7 @@ import { Accordion, AccordionItem } from "@nextui-org/accordion";
 import { CourseContext } from "@/assets/contexes/site/course.contex";
 import { memo, useContext, useState } from "react";
 import Session from "./Session";
+import DangerAlert from "@/assets/components/elems/DangerAlert";
 
 const Sessions = memo(() => {
   const course = useContext(CourseContext);
@@ -30,9 +31,8 @@ const Sessions = memo(() => {
       </AccordionItem>      
     </Accordion>
   ) : (
-    <div className="bg-red-100 dark:bg-red-200 text-red-900 sm:text-lg md:text-xl font-bold font-yekan px-4 py-8 rounded-lg shadow">
-      فعلا جلسه ای آپلود نشده است ...
-    </div>
+    <DangerAlert text="فعلا جلسه ای آپلود نشده است ..."/>
+    
   );
 });
 
