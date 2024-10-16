a60118da35d81d1152a4532b468ccee886a9e41e
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-10-16 20:01:13 +0330

    Edit ArticlePreview Page Codes

--- src/pages/site/ArticlePreview.tsx
+++ src/pages/site/ArticlePreview.tsx
@@ -2,7 +2,11 @@ import { memo } from 'react'
 
 const ArticlePreview = memo(() => {
   return (
-    <div>ArticlePreview</div>
+    <main className="container mt-14 mb-20">
+      <div className='grid xl:grid-cols-24 grid-cols-12 gap-6'>
+        
+      </div>
+    </main>
   )
 })
 
