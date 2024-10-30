78f209df10fcdd5f677f0a24a488cec2b79985ab
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-10-30 20:30:51 +0330

    refactor getting course with category codes

--- src/assets/hooks/shared/useCourses.tsx
+++ src/assets/hooks/shared/useCourses.tsx
@@ -1,11 +1,19 @@
 import { getCourses } from "@/assets/services/axios/requests/shared/courses"
+import { CourseBoxType } from "@/assets/types/share/course.type";
 import { useQuery } from "react-query"
 import { useParams } from "react-router-dom";
 
 const useCourses = () => {
   const params = useParams();
 
-  const {data: courses} = useQuery(["courses", params.category], getCourses)
+  const {data: courses} = useQuery(["courses"], getCourses, {
+    select: queryData => {
+      const selectedData: CourseBoxType[] = queryData as CourseBoxType[]
+      return params.category 
+        ? selectedData.filter(course => course.categoryID.name === params.category)
+        : selectedData
+    }
+  })
   
 
   return {courses}
