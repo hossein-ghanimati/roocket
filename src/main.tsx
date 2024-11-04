ea33f705f8aadc83d6992d8baf29fcc1eb0f8651
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-11-04 17:30:09 +0330

    Use hashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeContextProvider } from "./assets/contexts/site/theme.context";
 import AppWrapper from "./AppWrapper";
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
+    <HashRouter>
         <ThemeContextProvider>
           <AppWrapper />
         </ThemeContextProvider>
-    </BrowserRouter>
+    </HashRouter>
 );
